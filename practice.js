6854dbb4095d12116151dd3ca383487567212199
prince2662 <kaharprince45@gmail.com>
2024-10-07 18:32:21 +0530

    add Daily task

---
+++ practice.js
@@ -0,0 +1,10 @@
+var marks = [55,25,55.5,95.2]
+        
+///access value///
+
+        console.log(marks);
+        console.log(marks[0]);
+        console.log(marks[1]);
+        console.log(marks[2]);
+        console.log(marks[3]);
+        console.log(marks[4]);
